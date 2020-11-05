4738af00cad2a2c8363cee85f376739f2fd5b9f0
Florian Lenz <florian.lenz@enpower.de>
2020-11-05 20:54:44 +0100

    init

---
+++ LoopRefactoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopRefactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
